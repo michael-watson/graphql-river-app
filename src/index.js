2c949637645688fcfc7d8f767671d46978cad8ce
Michael Watson <michaelwatson93@gmail.com>
2019-05-31 13:27:13 -0700

    testing context function

--- src/index.js
+++ src/index.js
@@ -6,16 +6,30 @@ const UsgsRestSource = require('./datasources/usgsRestSource');
 const { ApolloServer } = require('apollo-server');
 
 const fs = require('fs');
-const path =require('path');
+const path = require('path');
 const typeDefs = [fs.readFileSync(path.join(__dirname, "./schema.graphql"), "utf8")];
+
+const favoriteSites = new FavoriteSites();
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 	dataSources: () => ({
-		favoriteSites: new FavoriteSites(),
+		favoriteSites: favoriteSites,
 		usgsSystem: new UsgsRestSource()
-	})
+	}),
+	engine: {
+		schemaTag: process.env['SCHEMA_TAG']
+	}
+	// context: async ({ req }) => {
+	// 	try {
+	// 		let test = await favoriteSites.getFavoriteSitesByEmail("michaelwatson93@gmail.com");
+	// 		console.log(req);
+	// 	} catch (err) {
+	// 		console.log(err);
+	// 	}
+	// 	return req;
+	// }
 });
 
 server.listen().then(({ url }) => {
